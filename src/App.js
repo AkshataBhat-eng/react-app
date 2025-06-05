bd3864e13d09fec0b71fadbb499e6fabc22a0123
Akshata R Bhat <akshata.bhat@probeinformation.com>
2025-06-05 10:37:23 +0530

    react day4

--- src/App.js
+++ src/App.js
@@ -2,8 +2,21 @@ import Footer from './Footer';
 import Greetings from './Greetings';
 import './App.css';
 import Clock from './Clock';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCount(prevCount => prevCount + 1);
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+
+
   return (
     <div className="App">
      <div>
@@ -12,6 +25,10 @@ function App() {
      </div>
      <div>
       <Greetings name='Akshata' message='Good Day!' />
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>
+        Increment
+      </button>
+      <div>{count}</div>
       <Footer />
      </div>
     </div>
